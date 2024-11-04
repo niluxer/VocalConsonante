baaff8f74e405a33aad7d986b7d14b493cce17bf
Jesús Sánchez <jesus.sanchez@itcelaya.edu.mx>
2024-11-04 08:53:04 -0600

    Valida letras y detecta tanto lentras en mayusculas como en minusculas

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -1,6 +1,6 @@
 public class VocalConsonante {
     public static void main(String[] args) {
-        char ch = args[0].charAt(0);
+        char ch = args[0].toLowerCase().charAt(0);
 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " es una vocal.");
